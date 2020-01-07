bf60842e8ae54144bf9d596d2d9a7e96ccf34699
Youssef Medaghri <ymedaghri@octo.com>
2020-01-07 16:09:28 +0100

    Ugly trivia avec un test de caractérisation et outil associé

---
+++ src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -0,0 +1,156 @@
+package com.adaptionsoft.games.uglytrivia;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Game {
+
+  public List<String> logs = new ArrayList<>();
+  ArrayList players = new ArrayList();
+  int[] places = new int[6];
+  int[] purses = new int[6];
+  boolean[] inPenaltyBox = new boolean[6];
+  LinkedList popQuestions = new LinkedList();
+  LinkedList scienceQuestions = new LinkedList();
+  LinkedList sportsQuestions = new LinkedList();
+  LinkedList rockQuestions = new LinkedList();
+  int currentPlayer = 0;
+  boolean isGettingOutOfPenaltyBox;
+
+  public Game() {
+    for (int i = 0; i < 50; i++) {
+      popQuestions.addLast("Pop Question " + i);
+      scienceQuestions.addLast(("Science Question " + i));
+      sportsQuestions.addLast(("Sports Question " + i));
+      rockQuestions.addLast(createRockQuestion(i));
+    }
+  }
+
+  public String createRockQuestion(int index) {
+    return "Rock Question " + index;
+  }
+
+  public boolean add(String playerName) {
+
+    players.add(playerName);
+    places[howManyPlayers()] = 0;
+    purses[howManyPlayers()] = 0;
+    inPenaltyBox[howManyPlayers()] = false;
+
+    log(playerName + " was added");
+    log("They are player number " + players.size());
+    return true;
+  }
+
+  public int howManyPlayers() {
+    return players.size();
+  }
+
+  public void roll(int roll) {
+    log(players.get(currentPlayer) + " is the current player");
+    log("They have rolled a " + roll);
+
+    if (inPenaltyBox[currentPlayer]) {
+      if (roll % 2 != 0) {
+        isGettingOutOfPenaltyBox = true;
+
+        log(players.get(currentPlayer) + " is getting out of the penalty box");
+        places[currentPlayer] = places[currentPlayer] + roll;
+        if (places[currentPlayer] > 11) { places[currentPlayer] = places[currentPlayer] - 12; }
+
+        log(players.get(currentPlayer) + "'s new location is " + places[currentPlayer]);
+        log("The category is " + currentCategory());
+        askQuestion();
+      } else {
+        log(players.get(currentPlayer) + " is not getting out of the penalty box");
+        isGettingOutOfPenaltyBox = false;
+      }
+
+    } else {
+
+      places[currentPlayer] = places[currentPlayer] + roll;
+      if (places[currentPlayer] > 11) { places[currentPlayer] = places[currentPlayer] - 12; }
+
+      log(players.get(currentPlayer) + "'s new location is " + places[currentPlayer]);
+      log("The category is " + currentCategory());
+      askQuestion();
+    }
+
+  }
+
+  private void askQuestion() {
+    if (currentCategory() == "Pop") { log(popQuestions.removeFirst()); }
+    if (currentCategory() == "Science") { log(scienceQuestions.removeFirst()); }
+    if (currentCategory() == "Sports") { log(sportsQuestions.removeFirst()); }
+    if (currentCategory() == "Rock") { log(rockQuestions.removeFirst()); }
+  }
+
+
+  private String currentCategory() {
+    if (places[currentPlayer] == 0) { return "Pop"; }
+    if (places[currentPlayer] == 4) { return "Pop"; }
+    if (places[currentPlayer] == 8) { return "Pop"; }
+    if (places[currentPlayer] == 1) { return "Science"; }
+    if (places[currentPlayer] == 5) { return "Science"; }
+    if (places[currentPlayer] == 9) { return "Science"; }
+    if (places[currentPlayer] == 2) { return "Sports"; }
+    if (places[currentPlayer] == 6) { return "Sports"; }
+    if (places[currentPlayer] == 10) { return "Sports"; }
+    return "Rock";
+  }
+
+  public boolean wasCorrectlyAnswered() {
+    if (inPenaltyBox[currentPlayer]) {
+      if (isGettingOutOfPenaltyBox) {
+        log("Answer was correct!!!!");
+        purses[currentPlayer]++;
+        log(players.get(currentPlayer) + " now has " + purses[currentPlayer] + " Gold Coins.");
+
+        boolean winner = didPlayerWin();
+        currentPlayer++;
+        if (currentPlayer == players.size()) { currentPlayer = 0; }
+
+        return winner;
+      } else {
+        currentPlayer++;
+        if (currentPlayer == players.size()) { currentPlayer = 0; }
+        return true;
+      }
+
+
+    } else {
+
+      log("Answer was corrent!!!!");
+      purses[currentPlayer]++;
+      log(players.get(currentPlayer) + " now has " + purses[currentPlayer] + " Gold Coins.");
+
+      boolean winner = didPlayerWin();
+      currentPlayer++;
+      if (currentPlayer == players.size()) { currentPlayer = 0; }
+
+      return winner;
+    }
+  }
+
+  public boolean wrongAnswer() {
+    log("Question was incorrectly answered");
+    log(players.get(currentPlayer) + " was sent to the penalty box");
+    inPenaltyBox[currentPlayer] = true;
+
+    currentPlayer++;
+    if (currentPlayer == players.size()) { currentPlayer = 0; }
+    return true;
+  }
+
+
+  private boolean didPlayerWin() {
+    return !(purses[currentPlayer] == 6);
+  }
+
+  public void log(Object message) {
+
+    logs.add((String)message);
+    System.out.println(message);
+  }
+}
---
+++ src/test/java/com/adaptionsoft/games/SomeTest.java
@@ -0,0 +1,187 @@
+package com.adaptionsoft.games;
+
+
+import com.adaptionsoft.games.uglytrivia.Game;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class SomeTest {
+
+  class GameWithSeam extends Game {
+
+    @Override
+    public void log(Object message) {
+      logs.add((String) message);
+    }
+  }
+
+  @Test
+  public void utilitaireDeCaracterisation() {
+
+
+    class RollWithResult {
+
+      int roll;
+      boolean answer;
+
+      public RollWithResult(int roll, boolean answer) {
+        this.roll = roll;
+        this.answer = answer;
+      }
+
+      @Override
+      public String toString() {
+        String answercall = (answer) ? "aGame.wasCorrectlyAnswered();" : "aGame.wrongAnswer();";
+        return "aGame.roll(" + roll + "); " + answercall;
+
+      }
+    }
+
+    boolean notAWinner;
+
+    List<RollWithResult> rollWithResults = new ArrayList<>();
+
+    Game aGame = new GameWithSeam();
+
+    aGame.add("Chet");
+    aGame.add("Pat");
+    aGame.add("Sue");
+
+    Random rand = new Random();
+
+    do {
+
+      int roll = rand.nextInt(5) + 1;
+
+      aGame.roll(roll);
+
+      if (rand.nextInt(9) == 7) {
+        notAWinner = aGame.wrongAnswer();
+        rollWithResults.add(new RollWithResult(roll, false));
+      } else {
+        notAWinner = aGame.wasCorrectlyAnswered();
+        rollWithResults.add(new RollWithResult(roll, true));
+      }
+
+
+    } while (notAWinner);
+
+    System.out.println("Entrées : ");
+    System.out.println("----------");
+    System.out.println(
+        rollWithResults.stream().map(RollWithResult::toString).collect(Collectors.joining("\n")));
+
+    System.out.println("");
+    System.out.println("Sorties : ");
+    System.out.println("----------");
+    System.out.println(aGame.logs.stream().collect(Collectors.joining("\n")));
+
+
+  }
+
+  @Test
+  public void aCompleteGame() {
+    // Given
+
+    Game aGame = new GameWithSeam();
+
+    aGame.add("Chet");
+    aGame.add("Pat");
+    aGame.add("Sue");
+
+    // When
+
+    aGame.roll(4);
+    aGame.wasCorrectlyAnswered();
+    aGame.roll(1);
+    aGame.wasCorrectlyAnswered();
+    aGame.roll(2);
+    aGame.wasCorrectlyAnswered();
+    aGame.roll(5);
+    aGame.wasCorrectlyAnswered();
+    aGame.roll(2);
+    aGame.wasCorrectlyAnswered();
+    aGame.roll(5);
+    aGame.wasCorrectlyAnswered();
+    aGame.roll(3);
+    aGame.wasCorrectlyAnswered();
+    aGame.roll(5);
+    aGame.wrongAnswer();
+    aGame.roll(3);
+    aGame.wasCorrectlyAnswered();
+    aGame.roll(2);
+    aGame.wasCorrectlyAnswered();
+    aGame.roll(4);
+    aGame.wasCorrectlyAnswered();
+    aGame.roll(4);
+    aGame.wasCorrectlyAnswered();
+    aGame.roll(1);
+    aGame.wrongAnswer();
+    aGame.roll(2);
+    aGame.wasCorrectlyAnswered();
+    aGame.roll(4);
+    aGame.wasCorrectlyAnswered();
+    aGame.roll(4);
+    aGame.wasCorrectlyAnswered();
+    aGame.roll(1);
+    aGame.wasCorrectlyAnswered();
+    aGame.roll(5);
+    aGame.wasCorrectlyAnswered();
+
+    // Then
+
+    Assertions.assertThat(aGame.logs.stream().collect(Collectors.joining("\n"))).isEqualTo(
+        "Chet was added\n" + "They are player number 1\n" + "Pat was added\n"
+            + "They are player number 2\n" + "Sue was added\n" + "They are player number 3\n"
+            + "Chet is the current player\n" + "They have rolled a 4\n"
+            + "Chet's new location is 4\n" + "The category is Pop\n" + "Pop Question 0\n"
+            + "Answer was corrent!!!!\n" + "Chet now has 1 Gold Coins.\n"
+            + "Pat is the current player\n" + "They have rolled a 1\n" + "Pat's new location is 1\n"
+            + "The category is Science\n" + "Science Question 0\n" + "Answer was corrent!!!!\n"
+            + "Pat now has 1 Gold Coins.\n" + "Sue is the current player\n"
+            + "They have rolled a 2\n" + "Sue's new location is 2\n" + "The category is Sports\n"
+            + "Sports Question 0\n" + "Answer was corrent!!!!\n" + "Sue now has 1 Gold Coins.\n"
+            + "Chet is the current player\n" + "They have rolled a 5\n"
+            + "Chet's new location is 9\n" + "The category is Science\n" + "Science Question 1\n"
+            + "Answer was corrent!!!!\n" + "Chet now has 2 Gold Coins.\n"
+            + "Pat is the current player\n" + "They have rolled a 2\n" + "Pat's new location is 3\n"
+            + "The category is Rock\n" + "Rock Question 0\n" + "Answer was corrent!!!!\n"
+            + "Pat now has 2 Gold Coins.\n" + "Sue is the current player\n"
+            + "They have rolled a 5\n" + "Sue's new location is 7\n" + "The category is Rock\n"
+            + "Rock Question 1\n" + "Answer was corrent!!!!\n" + "Sue now has 2 Gold Coins.\n"
+            + "Chet is the current player\n" + "They have rolled a 3\n"
+            + "Chet's new location is 0\n" + "The category is Pop\n" + "Pop Question 1\n"
+            + "Answer was corrent!!!!\n" + "Chet now has 3 Gold Coins.\n"
+            + "Pat is the current player\n" + "They have rolled a 5\n" + "Pat's new location is 8\n"
+            + "The category is Pop\n" + "Pop Question 2\n" + "Question was incorrectly answered\n"
+            + "Pat was sent to the penalty box\n" + "Sue is the current player\n"
+            + "They have rolled a 3\n" + "Sue's new location is 10\n" + "The category is Sports\n"
+            + "Sports Question 1\n" + "Answer was corrent!!!!\n" + "Sue now has 3 Gold Coins.\n"
+            + "Chet is the current player\n" + "They have rolled a 2\n"
+            + "Chet's new location is 2\n" + "The category is Sports\n" + "Sports Question 2\n"
+            + "Answer was corrent!!!!\n" + "Chet now has 4 Gold Coins.\n"
+            + "Pat is the current player\n" + "They have rolled a 4\n"
+            + "Pat is not getting out of the penalty box\n" + "Sue is the current player\n"
+            + "They have rolled a 4\n" + "Sue's new location is 2\n" + "The category is Sports\n"
+            + "Sports Question 3\n" + "Answer was corrent!!!!\n" + "Sue now has 4 Gold Coins.\n"
+            + "Chet is the current player\n" + "They have rolled a 1\n"
+            + "Chet's new location is 3\n" + "The category is Rock\n" + "Rock Question 2\n"
+            + "Question was incorrectly answered\n" + "Chet was sent to the penalty box\n"
+            + "Pat is the current player\n" + "They have rolled a 2\n"
+            + "Pat is not getting out of the penalty box\n" + "Sue is the current player\n"
+            + "They have rolled a 4\n" + "Sue's new location is 6\n" + "The category is Sports\n"
+            + "Sports Question 4\n" + "Answer was corrent!!!!\n" + "Sue now has 5 Gold Coins.\n"
+            + "Chet is the current player\n" + "They have rolled a 4\n"
+            + "Chet is not getting out of the penalty box\n" + "Pat is the current player\n"
+            + "They have rolled a 1\n" + "Pat is getting out of the penalty box\n"
+            + "Pat's new location is 9\n" + "The category is Science\n" + "Science Question 2\n"
+            + "Answer was correct!!!!\n" + "Pat now has 3 Gold Coins.\n"
+            + "Sue is the current player\n" + "They have rolled a 5\n"
+            + "Sue's new location is 11\n" + "The category is Rock\n" + "Rock Question 3\n"
+            + "Answer was corrent!!!!\n" + "Sue now has 6 Gold Coins.");
+  }
+}
